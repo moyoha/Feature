50b13b2114c7be5b2ea8c4134db1492052dfdc3f
mayoha <mayoha@163.com>
2024-08-05 23:20:47 +0800

    鸡尾酒排序

---
+++ sort/cocktail.js
@@ -0,0 +1,29 @@
+// 鸡尾酒排序
+function cocktail(arr) {
+  const len = arr.length;
+  for(let i = 0; i < len / 2; i++) {
+    let isSorted = true;
+    for(let j = i; j < len - 1 - i; j++) {
+      if(arr[j] > arr[j + 1]) {
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]]
+        isSorted = false;
+      }
+    }
+    if(isSorted) {
+      break;
+    }
+    isSorted = true;
+    for(let k = len - 1 - i; k > 0; k--) {
+      if(arr[k] < arr[k - 1]) {
+        [arr[k], arr[k - 1]] = [arr[k - 1], arr[k]]
+        isSorted = false;
+      }
+    }
+    if(isSorted) {
+      break;
+    }
+  }
+  return arr;
+}
+
+console.log(cocktail([2, 3, 4, 5, 6, 7, 8, 1]));
