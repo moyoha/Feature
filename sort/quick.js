b93b25ce08e84106e19ba318944fb1e0468c9a9a
mayoha <mayoha@163.com>
2024-08-12 23:35:57 +0800

    快排单边循环法

--- sort/quick.js
+++ sort/quick.js
@@ -6,7 +6,6 @@ function quick(arr, startIndex, endIndex) {
   const pivotIndex = partition(arr, startIndex, endIndex);
   quick(arr, startIndex, pivotIndex - 1)
   quick(arr, pivotIndex + 1, endIndex)
-  return arr;
 }
 
 function partition(arr, startIndex, endIndex) {
@@ -30,4 +29,35 @@ function partition(arr, startIndex, endIndex) {
 }
 
 const arr = [4, 7, 6, 5, 3, 2, 8, 1]
-console.log(quick(arr, 0, arr.length - 1));
+quick(arr, 0, arr.length - 1)
+console.log(arr);
+
+// 单边循环法
+function quick2(arr, startIndex, endIndex) {
+  if(startIndex >= endIndex) {
+    return
+  }
+  const pivotIndex = partition2(arr, startIndex, endIndex)
+  quick2(arr, startIndex, pivotIndex - 1)
+  quick2(arr, pivotIndex + 1, endIndex)
+}
+
+function partition2(arr, startIndex, endIndex) {
+  const pivot = arr[startIndex]
+  let mark = startIndex
+  let left = startIndex + 1
+  while (left <= endIndex) {
+    if(arr[left] >= pivot) {
+      left++
+    } else {
+      mark++
+      [arr[mark], arr[left]] = [arr[left], arr[mark]]
+      left++
+    }
+  }
+  [arr[startIndex], arr[mark]] = [arr[mark], arr[startIndex]]
+  return mark
+}
+
+// quick2(arr, 0, arr.length - 1)
+// console.log(arr);
