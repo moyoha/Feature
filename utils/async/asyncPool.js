2f8a06c968b546e1ba1a1bfb53a911bfe11606ca
mayoha <mayoha@163.com>
2025-04-01 01:36:17 +0800

    完成并发封装

--- utils/async/asyncPool.js
+++ utils/async/asyncPool.js
@@ -8,18 +8,27 @@ async function asyncPoolAll(iterable, iteratorFn, concurrency = 3) {
     rv.push(p);
     executing.add(p);
     const clean = () => executing.delete(p);
-    p.finally(clean);
+    p.then(clean).catch(clean);
+    // 如果想要 promise 在被 reject 后仍然能够继续执行其他异步任务,需要在此处进行错误处理 
     if (executing.size >= concurrency) {
-      try {
-        await Promise.race(executing);
-      } catch (error) {
-        console.error(error);
-      }
+      await Promise.race(executing);
     }
   }
 
   return Promise.allSettled(rv);
 }
+
+// 测试并发
+async function teatAll() {
+  try {
+    const res = await asyncPoolAll([500, 200, 100], async (ms) => delay(ms, ms), 2);
+    console.log(res);
+  } catch (error) {
+    console.error({ error });
+  }
+}
+
+// teatAll();
 
 // 能提供并发控制,并且能够流式处理任务
 async function* asyncPoolStream(iterable, iteratorFn, concurrency = 3) {
@@ -30,24 +39,17 @@ async function* asyncPoolStream(iterable, iteratorFn, concurrency = 3) {
 
     executing.add(p);
     const clean = () => executing.delete(p);
-    p.finally(clean);
+    p.then(clean).catch(clean);
 
+    // 如果想要 promise 在被 reject 后仍然能够继续执行其他异步任务,需要在此处进行错误处理 
     if (executing.size >= concurrency) {
-      try {
-        yield await Promise.race(executing);
-      } catch (error) {
-        console.error(error);
-      }
+      yield await Promise.race(executing);
     }
   }
 
   // 避免有任务未执行
   while (executing.size) {
-    try {
-      yield await Promise.race(executing);
-    } catch (error) {
-      console.error(error);
-    }
+    yield await Promise.race(executing);
   }
 }
 
@@ -66,20 +68,8 @@ async function testStream() {
       console.log(result);
     }
   } catch (error) {
-
+    console.error({ error });
   }
 }
 
-// testStream();
-
-// 测试并发
-async function teatAll() {
-  try {
-    const res = await asyncPoolAll([500, 200, 100], async (ms) => delay(ms, ms), 2);
-    console.log(res);
-  } catch (error) {
-
-  }
-}
-
-teatAll();
+testStream();
